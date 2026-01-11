4a0c13a6fd0524f644e2e2c874a364379094f956
рокс__) <99092643+rocksJS@users.noreply.github.com>
2026-01-12 01:57:40 +0300

    first commit

---
+++ src/main/java/com/entity/Entity.java
@@ -0,0 +1,34 @@
+package com.entity;
+
+import net.fabricmc.api.ModInitializer;
+// import net.minecraft.server.commands.
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Entity implements ModInitializer {
+	public static final String MOD_ID = "entity";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(Commands.literal("heal")
+					.requires(source -> source.hasPermission(0)) // Только OP (уровень 2+)
+					.executes(context -> executeHeal(context.getSource())));
+		});
+	}
+
+	private int executeHeal(CommandSourceStack source) {
+		source.getPlayer().setHealth(20);
+		source.sendSuccess(() -> net.minecraft.network.chat.Component.literal("§aВы исцелены!"), false);
+		return 1;
+	}
+}
